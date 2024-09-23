7b34d82b8b2048a759533acc83d2ec3a83582680
Atika Istiqomah <48738106+atikaistiqomah@users.noreply.github.com>
2024-09-23 14:11:03 +0700

    Update db_set.py

--- db_set.py
+++ db_set.py
@@ -5,7 +5,7 @@ import json
 import pandas as pd
 
 def get_db_connection():
-    conn = sqlite3.connect('db/new_data.db')
+    conn = sqlite3.connect('new_data.db')
     conn.row_factory = sqlite3.Row
     return conn
 
